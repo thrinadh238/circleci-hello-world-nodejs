84199361b9d4798b11946613101f70f9efebb25b
thrinadh238 <thrinadh238@gmail.com>
2019-11-24 21:47:31 +0530

    Update fargate-service.sh

--- containers/fargate-service.sh
+++ containers/fargate-service.sh
@@ -1,7 +1,11 @@
-#!/bin/bash
+#!/bin/sh
+# set -o errexit
 
-if [[ "$(aws ecs list-services --cluster default 2> /dev/null | grep serviceArn)" == "serviceArn" ]]; then
-  echo "service already created!"
-else
+case "$(aws ecs list-services --cluster default 2> /dev/null | grep serviceArn)" in
+    *serviceArn*)
+	    echo "ServiceArn Exists"
+        ;;
+     *)
   aws ecs create-service --cluster default --service-name nginx --task-definition sample-fargate --launch-type "FARGATE"  --network-configuration "awsvpcConfiguration={subnets=[${AWS_SUBNET_ID}],securityGroups=[sg-0e11ea763d1da77fe],assignPublicIp=ENABLED}" --cli-input-json file:///root/project/containers/fargate-service.json
-fi
+    ;;
+esac
